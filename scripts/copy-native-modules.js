95ff93f1b8f13a54ebb9993dc6a7fade9201fe33
Arne Schmid <arne.schmid@arm.com>
2026-03-25 11:27:33 +0100

    Enhance CI workflow and package configuration by adding extension version to artifact name, updating dependencies, and including additional native modules for packaging.

--- scripts/copy-native-modules.js
+++ scripts/copy-native-modules.js
@@ -2,7 +2,27 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const modules = ['serialport', 'usb'];
+const modules = [
+    'serialport',
+    'usb',
+    '@serialport/binding-mock',
+    '@serialport/bindings-cpp',
+    '@serialport/bindings-interface',
+    '@serialport/parser-byte-length',
+    '@serialport/parser-cctalk',
+    '@serialport/parser-delimiter',
+    '@serialport/parser-inter-byte-timeout',
+    '@serialport/parser-packet-length',
+    '@serialport/parser-readline',
+    '@serialport/parser-ready',
+    '@serialport/parser-regex',
+    '@serialport/parser-slip-encoder',
+    '@serialport/parser-spacepacket',
+    '@serialport/stream',
+    'ms',
+    'debug',
+    'node-gyp-build',
+];
 const outNodeModules = path.join(__dirname, '..', 'out', 'node_modules');
 
 fs.ensureDirSync(outNodeModules);
